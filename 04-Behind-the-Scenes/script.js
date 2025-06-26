4417b6e9b4d9e56b4682c869a214546826638260
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-06-27 00:29:46 +0530

    Scoping in practice

--- 04-Behind-the-Scenes/script.js
+++ 04-Behind-the-Scenes/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2025 - birthYear;
+
+  function pritAge() {
+    let output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Meet';
+
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT!';
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  pritAge();
+
+  return age;
+}
+
+const firstName = 'Rahul';
+calcAge(1991);
+// console.log(age);
+// pritAge();
