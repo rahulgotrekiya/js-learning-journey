4693b8d2ca93ab24c84cbe74864d899c665f32d8
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-06-27 01:44:46 +0530

    Hoisting and TDZ in practice

--- 04-Behind-the-Scenes/script.js
+++ 04-Behind-the-Scenes/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
   const age = 2025 - birthYear;
 
@@ -37,3 +37,51 @@ const firstName = 'Rahul';
 calcAge(1991);
 // console.log(age);
 // pritAge();
+*/
+
+/* -------------------------------- */
+// Hoisting and TDZ in Practice
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'rahul';
+let job = 'student';
+const year = 2005;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+console.log(undefined);
+if (!numProduct) deleteShoppingCart();
+
+var numProduct = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.y);
