6ae1ac7a63c3282990f4560a2c6a024ecf5b318f
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-06-29 23:00:08 +0530

    Maps: Fundamentals

--- 05-Data-Structures-Operators/script.js
+++ 05-Data-Structures-Operators/script.js
@@ -55,6 +55,43 @@ const restaurant = {
   },
 };
 
+// Maps: Fundamentals
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugalas'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+/*
+
+//----------------------//
+
 // Sets
 
 const ordreSet = new Set([
@@ -91,6 +128,8 @@ console.log(
 );
 
 console.log(new Set('rahulgotrekiya').size);
+
+*/
 
 //----------------------//
 
