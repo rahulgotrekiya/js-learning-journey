49bf07f04aeaca288ae825dae2fe9980bb1f7730
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-06-29 14:27:00 +0530

    Optional chaining (.?)

--- 05-Data-Structures-Operators/script.js
+++ 05-Data-Structures-Operators/script.js
@@ -55,7 +55,38 @@ const restaurant = {
   },
 };
 
-/* 
+// Optional chaining (.?)
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+// const users = [{ name: 'rahul', email: 'hello@rahul.io' }];
+const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) console.log(users[0].name);
+else console.log('User array empty');
+
+/*
+
+//----------------------//
 
 // Looping Arrays: The for-of Loop
 
