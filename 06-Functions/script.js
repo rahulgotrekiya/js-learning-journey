2eadd384c2798cd794e7f52daaa14bbf649ed06f
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-07-02 22:53:32 +0530

    How Passing Arguments Works: Value vs. Reference

--- 06-Functions/script.js
+++ 06-Functions/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*
+
 // Default Parameters
 
 const bookings = [];
@@ -27,3 +29,38 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+
+*/
+
+// How Passing Arguments Works: Value vs. Reference
+
+const flight = 'Lh234';
+const rahul = {
+  name: 'Rahul Gotrekiya',
+  passport: 343452123224,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport === 343452123224) {
+    alert('Check In ');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, rahul);
+// console.log(flight);
+// console.log(rahul);
+
+// // Is the same ad doing...
+// const flightNum = flight;
+// const passenger = rahul;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(rahul);
+checkIn(flight, rahul);
