199261ed0b9ec7dbc3ca736a0ff9e45d9900701c
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-07-06 01:22:53 +0530

    Immediately Invoked Function Expressions (IIFE)

--- 06-Functions/script.js
+++ 06-Functions/script.js
@@ -279,6 +279,8 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 GOOD LUCK 😀
 */
 
+/*
+
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
@@ -320,3 +322,32 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
 // [5, 2, 3]
 // [1, 5, 3, 9, 6, 1]
+
+*/
+
+//--------------------------------//
+
+// Immediately Invoked Function Expressions (IIFE)
+
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+
+// console.log(isPrivate);
+console.log(notPrivate);
