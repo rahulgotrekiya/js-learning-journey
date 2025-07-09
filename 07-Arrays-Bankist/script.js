ceb06726087a901102f906680d5c78158ef02878
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-07-09 19:17:39 +0530

    The find Method

--- 07-Arrays-Bankist/script.js
+++ 07-Arrays-Bankist/script.js
@@ -461,6 +461,8 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
+/*
+
 const calcAverageHumanAge2 = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   const adults = humanAges.filter(age => age >= 18);
@@ -485,3 +487,18 @@ const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
 console.log(avg1, avg2);
+
+*/
+
+// --------------- //
+// The find Method
+// --------------- //
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
