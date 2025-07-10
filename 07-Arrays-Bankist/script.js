db57e658016502218675054c92508847860f8bb3
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-07-10 19:40:15 +0530

    The findIndex Method

--- 07-Arrays-Bankist/script.js
+++ 07-Arrays-Bankist/script.js
@@ -195,6 +195,32 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+// -------------------- //
+// The findIndex Method
+// -------------------- //
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+    // .indexOf(23)
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
