ee80c3a6ef9a0c723c7215c3b0e10b32723bd807
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-07-23 23:19:12 +0530

    Numeric Separators

--- 08-Numbers-Dates-Timers-Bankist/script.js
+++ 08-Numbers-Dates-Timers-Bankist/script.js
@@ -349,6 +349,8 @@ console.log(+(2.345).toFixed(2));
 // The Remainder Operator
 /* ---------------------- */
 
+/*
+
 console.log(5 % 2);
 console.log(5 / 2); // 5 = 2 * 2 + 1
 
@@ -374,3 +376,25 @@ labelBalance.addEventListener('click', function () {
     if (i % 3 === 0) row.style.backgroundColor = 'blue';
   });
 });
+
+*/
+
+/* ------------------ */
+// Numeric Separators
+/* ------------------ */
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee2 = 15_00;
+const transferFee1 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000'));
+console.log(parseInt('230_000'));
