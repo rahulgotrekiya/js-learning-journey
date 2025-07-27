a1366848844fd9e7eb8d6dd555773819a2d5a64a
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-07-27 11:27:33 +0530

    Implementing Smooth Scrolling

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -34,6 +34,8 @@ document.addEventListener('keydown', function (e) {
 // Selecting, Creating, and Deleting Elements
 /* ------------------------------------------ */
 
+/*
+
 // Selecting the Elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -71,9 +73,13 @@ document
     message.parentElement.removeChild(message);
   });
 
+*/
+
 /* ------------------------------ */
 // Styles, Attributes and Classes
 /* ------------------------------ */
+
+/*
 
 // Styles
 message.style.backgroundColor = '#37383d';
@@ -120,3 +126,49 @@ logo.classList.contains('c'); // not includes
 
 // Don't use
 // logo.className = 'Rahul';
+
+*/
+
+/* ------------------------------ */
+// Implementing Smooth Scrolling
+/* ------------------------------ */
+
+/*
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll(X,Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+/*
+
+/* --------------------------------- */
+// Type of Events and Event Handlers
+/* --------------------------------- */
