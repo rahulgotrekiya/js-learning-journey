5c0f6c616e7a2525cb27dace8794278b6b42eefe
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-08-05 22:42:04 +0530

    Tabbed component

--- 09-Advanced-DOM-Bankist/script.js
+++ 09-Advanced-DOM-Bankist/script.js
@@ -91,6 +91,33 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+/* ---------------- */
+// Tabbed component
+/* ---------------- */
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Gaurd clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Active contenet area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
 /* ------------------------------------------ */
 // Selecting, Creating, and Deleting Elements
 /* ------------------------------------------ */
@@ -248,6 +275,8 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 // DOM Traversing
 /* -------------- */
 
+/*
+
 const h1 = document.querySelector('h1');
 
 // Going downwards: child
@@ -277,3 +306,5 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
+
+*/
